a6db285b1166b6c15333b95fa406e19478f9345e
marigopal <88581007+marigopal@users.noreply.github.com>
2021-11-29 18:12:46 +0530

    OTP Update

---
+++ include/update_otp.php
@@ -0,0 +1,27 @@
+<?php
+
+//include('../../include/lib_page.php');
+$isNew = $_POST['isNew'];
+$minutes_to_add = 5;
+$time = new DateTime();
+$time->add(new DateInterval('PT' . $minutes_to_add . 'M'));
+$otp_expiration_on = $time->format('Y-m-d H:i:s');
+$new_otp = random_strings();
+if ($isNew === 'true') {
+    $query = "";
+} else if ($isNew === 'false') {
+    $user_id = $_POST['user_id'];
+    $query = "UPDATE `retailpro_db`.`tbl_user` SET `otp` = '$new_otp',`otp_expirationon` = '$otp_expiration_on' WHERE `user_id` = '$user_id'";
+}
+echo $query;exit();
+if ($result = $con->query($query)) {
+    echo "1";
+} else {
+    echo "0";
+}
+
+function random_strings() {
+    $str_result = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    return substr(str_shuffle($str_result), 0, 8);
+}
+?>
--- register/js/_index.js
+++ register/js/_index.js
@@ -1,114 +1,118 @@
-var gloabl_url= "http://retailpro.enflowapp.com/";
-$( document ).ready(function() {
+var gloabl_url = "http://retailpro.enflowapp.com/";
+$(document).ready(function () {
     get_language();
 });
 function get_language()
 {
     $.ajax
-    ({
-        type: "POST",
-        url: gloabl_url +"api/get_language.php",
-        data: {},
-        dataType: 'json',
-        success: function (result)
-        {
-            var status = result.status;
-            if(status == 200)
-            {
-               var response_length = result.response.length;
-               strHTML = "";
-               i=0;
-               for (var i = 0; i < response_length; i++)
+            ({
+                type: "POST",
+                url: gloabl_url + "api/get_language.php",
+                data: {},
+                dataType: 'json',
+                success: function (result)
                 {
+                    var status = result.status;
+                    if (status == 200)
+                    {
+                        var response_length = result.response.length;
+                        strHTML = "";
+                        i = 0;
+                        for (var i = 0; i < response_length; i++)
+                        {
                             var language_id = result.response[i]['language_id'];
                             var language_name = result.response[i]['language_name'];
-                            strHTML += "<div class='form-group'><input type='radio' name='input_language' id='input_language' class='agree-term input_language' value= '"+language_id+"'onclick='assign_langval(this.value)'/><label for='agree-term' class='label-agree-term'><span><span></span></span>"+ language_name + "</label></div>";
+                            strHTML += "<div class='form-group'><input type='radio' name='input_language' id='input_language' class='agree-term input_language' value= '" + language_id + "'onclick='assign_langval(this.value)'/><label for='agree-term' class='label-agree-term'><span><span></span></span>" + language_name + "</label></div>";
+                        }
+                        strHTML += "<span style='color:red' id='lang_notification'></span><div class='form-group form-button'><input type='button' name='language_button' id='language_button' class='form-submit' value='Next' onclick = 'language_button()'/></div>";
+                        $("#language_selection").append(strHTML);
+                    }
                 }
-                strHTML += "<span style='color:red' id='lang_notification'></span><div class='form-group form-button'><input type='button' name='language_button' id='language_button' class='form-submit' value='Next' onclick = 'language_button()'/></div>";
-                $("#language_selection").append(strHTML);
-            }
-        }
-    });
+            });
 }
 function assign_langval(lang)
 {
     $("#lang_value").val(lang);
-    $("#lang_notification").attr("hidden",true);
+    $("#lang_notification").attr("hidden", true);
 }
 function language_button()
 {
     var lang_val = $("#lang_value").val();
-    if(lang_val == '')
+    if (lang_val == '')
     {
         $("#lang_notification").html("Please select one Language");
-    }else{
-            $("#language_selection").attr("hidden",true);
-            $("#phonenumber_selection").removeAttr('hidden');
-         }   
+    } else {
+        $("#language_selection").attr("hidden", true);
+        $("#phonenumber_selection").removeAttr('hidden');
+    }
 }
-$("#validate_mobno").click(function(){
+$("#validate_mobno").click(function () {
     var mobileno = $("#input_mobileno").val();
-    if(mobileno == '')
+    if (mobileno == '')
     {
         $("#mob_notification").html("Please Enter your Mobile Number");
-    }else{
+    } else {
         var mobArr = {'mobilenumber': mobileno};
         $.ajax
-        ({
-            type: "POST",
-            url: gloabl_url +"api/validate_user.php",
-            data: JSON.stringify(mobArr),
-            dataType: 'json',
-            success: function (result)
-            {
-                var status = result.status;
-                if(status == 200)
-                {
-                    var response_length = result.response.length;
-                    i=0;
-                    for (var i = 0; i < response_length; i++)
+                ({
+                    type: "POST",
+                    url: gloabl_url + "api/validate_user.php",
+                    data: JSON.stringify(mobArr),
+                    dataType: 'json',
+                    success: function (result)
+                    {
+                        var status = result.status;
+                        if (status == 200)
                         {
-                                    var is_blocked = result.response[i]['is_blocked'];
-                                    var is_releaved = result.response[i]['is_releaved'];
-                                    var is_deleted = result.response[i]['is_deleted'];
-                                    var is_admin = result.response[i]['is_admin'];
-                                    var is_active = result.response[i]['is_active'];
-                                    var user_id = result.response[i]['user_id'];
-                                    isNew = false;
-                                    if(is_blocked == 1)
-                                    {
-                                        $("#mob_notification").html("User Blocked. Please contact Administrator.");
-                                    }else if(is_releaved == 1)
-                                    {
-                                        $("#mob_notification").html("User Releaved. Please contact Administrator.");
-                                    }else if(is_active == 1)
-                                    {
-                                        $("#otp_div").removeAttr('hidden');
-                                        generate_otp(user_id,isNew);
-
-                                        
-                                    }
-                                    
-                                    
-                                    
+                            var response_length = result.response.length;
+                            i = 0;
+                            for (var i = 0; i < response_length; i++)
+                            {
+                                var is_blocked = result.response[i]['is_blocked'];
+                                var is_releaved = result.response[i]['is_releaved'];
+                                var is_deleted = result.response[i]['is_deleted'];
+                                var is_admin = result.response[i]['is_admin'];
+                                var is_active = result.response[i]['is_active'];
+                                var user_id = result.response[i]['user_id'];
+                                isNew = false;
+                                if (is_blocked == 1)
+                                {
+                                    $("#mob_notification").html("User Blocked. Please contact Administrator.");
+                                } else if (is_releaved == 1)
+                                {
+                                    $("#mob_notification").html("User Releaved. Please contact Administrator.");
+                                } else if (is_active == 1)
+                                {
+                                    $("#otp_div").removeAttr('hidden');
+                                    generate_otp(user_id);
+                                }
+                            }
+                        } else if (status == 201)
+                        {
+                            
                         }
-                        
-                }
-                
-            }
-        });
+                    }
+                });
     }
 });
-function generate_otp(userid,isNew)
+function generate_otp(userid)
 {
     $.ajax
-    ({
-    type: "POST",
-    url: gloabl_url +"include/otp.php",
-    data: {user_id: userid,isNew: isNew},
-    dataType: 'json',
-    success: function (result)
-    {
-    }
-});
+            ({
+                type: "POST",
+//                url: gloabl_url + "include/update_otp.php",
+                url: "../../include/update_otp.php",
+                data: {user_id: userid},
+                dataType: 'json',
+                success: function (result)
+                {
+                }
+            });
+}
+function create_user()
+{
+    var lang_val = $("#lang_value").val();
+    var input_mobileno = $("#input_mobileno").val();
+
+
 }
